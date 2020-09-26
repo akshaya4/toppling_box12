9538a578dfbb115d570a708b0227ba6a6a03b490
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-26 14:49:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,15 +4,15 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 //var ground, ball;
-var box1,box2;
+var box1,box2,ground;
 
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
 
-    box1=new Box(100,100,50,50);
-    box2=new Box(100,50,50,50);
+  box1 = new Box(200,300,50,50);
+    box2 = new Box(240,100,50,100); 
     ground=new Ground(200,380,400,20);
     /*var ground_options ={
         isStatic: true
